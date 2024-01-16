324c4d3fb375b026915bdd33a428da5979f053fc
agungrhiz <39258850+agungrhiz@users.noreply.github.com>
2024-01-16 14:04:19 +0700

    swap char init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Kata : ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Output : " + SwapChar(input));
+            Console.ReadLine();
+        }
+
+        static string SwapChar(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            for (int i = 0; i < charArray.Length; i += 2)
+            {
+                if (i + 1 < charArray.Length)
+                {
+                    char temp = charArray[i];
+                    charArray[i] = charArray[i + 1];
+                    charArray[i + 1] = temp;
+                }
+            }
+            return new string(charArray);
+        }        
+    }
+}
